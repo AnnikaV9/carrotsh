c82594f818cdf9fa36a982c1079d10d8755fcaf8
V9 <68383195+AnnikaV9@users.noreply.github.com>
2021-12-04 14:24:07 +0800

    reflected config changes

--- setpass.py
+++ setpass.py
@@ -12,7 +12,7 @@ else:
     config_file = open("config.json", "r")
     config = json.load(config_file)
     config_file.close()
-    kdf = Scrypt(salt=config["salt"].encode(), length=32, n=2**14, r=8, p=1)
+    kdf = Scrypt(salt=config["password_auth_options"]["salt"].encode(), length=32, n=2**14, r=8, p=1)
     password_file = open("shadow", "wb")
     password_file.write(kdf.derive(password.encode()))
     password_file.close()
