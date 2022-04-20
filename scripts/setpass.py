7c144fd3bb972b919c4799886bfae3c6d4e24714
V9 <68383195+AnnikaV9@users.noreply.github.com>
2022-04-20 16:15:13 +0800

    Organizing

---
+++ scripts/setpass.py
@@ -0,0 +1,19 @@
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+import getpass
+import json
+import sys
+
+password = getpass.getpass("New password: ")
+if getpass.getpass("Confirm password: ") != password:
+    print("Error: Passwords do not match.")
+    sys.exit(1)
+
+else:
+    config_file = open("config.json", "r")
+    config = json.load(config_file)
+    config_file.close()
+    kdf = Scrypt(salt=config["password_auth_options"]["salt"].encode(), length=32, n=2**14, r=8, p=1)
+    password_file = open("login/password", "wb")
+    password_file.write(kdf.derive(password.encode()))
+    password_file.close()
+    print("Password saved.")
