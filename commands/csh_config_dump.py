273f48e6110e3a65a149259884f0e1ad6b9be347
carrot <carrot.moncher@gmail.com>
2022-05-09 09:44:21 +0800

    config file extension

--- commands/csh_config_dump.py
+++ commands/csh_config_dump.py
@@ -2,7 +2,7 @@ import yaml
 import json
 
 def main(args):
-    config_file = open("config.json", "r")
+    config_file = open("config.yaml", "r")
     config = yaml.safe_load(config_file)
     config_file.close()
     print(json.dumps(config, indent=4))
