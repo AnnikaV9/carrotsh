8b5cbcc3880c2ed603642bed9e18afca762e6f7d
carrot <carrot.moncher@gmail.com>
2022-05-09 09:43:14 +0800

    Use pyyaml to load config

--- commands/csh_config_dump.py
+++ commands/csh_config_dump.py
@@ -1,7 +1,8 @@
+import yaml
 import json
 
 def main(args):
     config_file = open("config.json", "r")
-    config = json.load(config_file)
+    config = yaml.safe_load(config_file)
     config_file.close()
     print(json.dumps(config, indent=4))
