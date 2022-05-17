6c607bd5e492ff5d00a1c902e1d2c59b8af576f0
carrot <carrot.moncher@gmail.com>
2022-05-17 17:12:34 +0800

    Style update

--- commands/csh_version.py
+++ commands/csh_version.py
@@ -1,7 +1,10 @@
 import json
 
-def main(args):
+
+def main(args: list) -> None:
     package_file = open("package.json", "r")
     version = json.load(package_file)["version"]
     package_file.close()
     print("carrotsh v{}".format(version))
+
+    return
