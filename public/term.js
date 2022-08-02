3688c8d4682c77d24253c5c49490068ff6014ba5
carrot <carrot.moncher@gmail.com>
2022-08-02 18:58:25 +0800

    Update term.js

--- public/term.js
+++ public/term.js
@@ -1,7 +1,7 @@
-const term = new Terminal({theme:{background:"#181a1b"}});
-const socket = new WebSocket(`${document.location.protocol==="http:"?"ws":"wss"}://${document.location.host }/ws/`);
-const websocketAddon = new AttachAddon.AttachAddon(socket);
-const resizeAddon = new FitAddon.FitAddon();
+const term=new Terminal({theme:{background:"#181a1b"}});
+const socket=new WebSocket(`${document.location.protocol==="http:"?"ws":"wss"}://${document.location.host }/ws/`);
+const websocketAddon=new AttachAddon.AttachAddon(socket);
+const resizeAddon=new FitAddon.FitAddon();
 term.loadAddon(websocketAddon);
 term.loadAddon(resizeAddon);
 term.open(document.getElementById("terminal"));
